8daa777fb4b2f457f58726c9a525f87e32a29b38
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2021-09-10 17:56:15 +0800

    add meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,9 @@ export default function Main() {
 					rel="stylesheet"
 					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 				/>
-				<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			</Head>
-			<body className="bg-white relative">
+			<body className="bg-white relative w-screen">
 				<header>
 					<nav className="p-6">
 						<div className="flex justify-between items-center">
